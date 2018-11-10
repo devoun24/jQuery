70537a87ab41437ad99e8ff3b9c2e7ca5e1845eb
Devoun Cetoute <devoun.c.cetoute@gmail.com>
2018-11-09 23:06:15 -0500

    first commit

---
+++ project/scripts/main.js
@@ -0,0 +1,48 @@
+$(document).ready(() => {
+
+  $('#foppresenting').on('mouseenter', () => {
+    $('.bigboy').attr('src', 'images/foppresenting.jpg');
+  });
+
+  $('#commissionworking').on('mouseenter', () => {
+    $('.bigboy').attr('src', 'images/commissionworking.jpg');
+  });
+
+  $('#chieftalk').on('mouseenter', () => {
+    $('.bigboy').attr('src', 'images/chieftalk.jpg');
+  });
+
+  $('#conferenceroomwatching').on('mouseenter', () => {
+    $('.bigboy').attr('src', 'images/conferenceroomwatching.jpg');
+  });
+
+  $('#breakroomwatching').on('mouseenter', () => {
+    $('.bigboy').attr('src', 'images/breakroomwatching.jpg');
+  });
+
+  $('#lobbywatching').on('mouseenter', () => {
+    $('.bigboy').attr('src', 'images/lobbywatching.jpg');
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
